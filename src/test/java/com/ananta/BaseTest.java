5328fea364ee6ac61a028a4e33731c9159d0ef87
Ananta Widyaswara <anantawidy@gmail.com>
2024-11-21 09:25:09 +0700

    Update Code

--- src/test/java/com/ananta/BaseTest.java
+++ src/test/java/com/ananta/BaseTest.java
@@ -9,6 +9,7 @@ public class BaseTest {
     protected static WebDriver driver;
 
     protected  void getDriver(){
+        // added manual driver
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
